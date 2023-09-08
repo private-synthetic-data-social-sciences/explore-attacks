cd23acf846951286992a07e41cc9f7f53430aa20
Flavio Hafner <f.hafner@esciencecenter.nl>
2023-09-08 11:29:14 +0200

    make dp cgan generator work

--- src/generator_dp_cgans.py
+++ src/generator_dp_cgans.py
@@ -1,34 +1,54 @@
-"""Sample code to train and sample from a small DP-CGANS model"""
+#!/usr/bin/env python
+"""Generate synthetic data with DP-CGANS model"""
+
+
+import sys
 
 from dp_cgans import DP_CGAN
-import pandas as pd
+import pandas as pd 
+import io 
 
-import numpy as np 
 
-np.random.seed(1)
+def read_data():
+    "Read a csv from stdin"
+    data = ""
+    for line in sys.stdin:
+        data += line
+    df = pd.read_csv(io.StringIO(data), sep=",", encoding="utf-8")
+    return df
 
-tabular_data=pd.read_csv("../datasets/Adult/Real/real_adult_data.csv")
 
-tabular_data = tabular_data.sample(n=1000)
+def generate_data(df, n):
+    """Train a generator on the dataframe df and return a synthetic dataset of size n"""
+    model = DP_CGAN(
+        epochs=100, # number of training epochs
+        batch_size=200, # the size of each batch
+        log_frequency=True,
+        verbose=True,
+        generator_dim=(128, 128, 128),
+        discriminator_dim=(128, 128, 128),
+        generator_lr=2e-4, 
+        discriminator_lr=2e-4,
+        discriminator_steps=1, 
+        private=False,
+    )
 
-# We adjusted the original CTGAN model from SDV. Instead of looking at the distribution of individual variable, we extended to two variables and keep their corrll
-model = DP_CGAN(
-   epochs=100, # number of training epochs
-   batch_size=200, # the size of each batch
-   log_frequency=True,
-   verbose=True,
-   generator_dim=(128, 128, 128),
-   discriminator_dim=(128, 128, 128),
-   generator_lr=2e-4, 
-   discriminator_lr=2e-4,
-   discriminator_steps=1, 
-   private=False,
-)
+    model.fit(df)
+    syn_data = model.sample(n)
+    return syn_data
 
-print("Start training model")
-model.fit(tabular_data)
-model.save("generator.pkl")
+def main(n):
+    """Generate synthetic data from an input dataset"""
+    df = read_data()
+    syn_data = generate_data(df, n)
+    # print to console 
+    print(syn_data.to_csv(None, index=False))
 
-# Generate 100 synthetic rows
-syn_data = model.sample(100)
-syn_data.to_csv("syn_data_file.csv")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        n_samples = int(sys.argv[1])
+    else:
+        raise RuntimeError("Need exactly one argument.")
+    
+    main(n_samples)
